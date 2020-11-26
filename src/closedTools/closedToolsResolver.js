7ef1a66237e05b3e94a9dd25415fbe3555cd775d
danielMorenoAlonso <daniel.mor.alon@gmail.com>
2020-11-25 21:42:51 -0600

    Revert "feat: Added a first version of dtt service"
    This reverts commit c2caaa74de8f148a2c087147385eabcd221220ea.

--- src/closedTools/closedToolsResolver.js
+++
@@ -1,6 +0,0 @@
-import {mergeResolvers} from 'merge-graphql-schemas';
-// Import each resolver file
-import {dttResolver} from './dttService/dttResolver';
-
-// merge all resolvers files and exports them to index
-export const resolvers = mergeResolvers([dttResolver]);

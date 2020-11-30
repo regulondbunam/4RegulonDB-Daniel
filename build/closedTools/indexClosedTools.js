7db2dcfbf790e0c6354108eeb4aa0513039d93d4
danielMorenoAlonso <daniel.mor.alon@gmail.com>
2020-11-30 15:58:59 -0600

    testing heroku server

--- build/closedTools/indexClosedTools.js
+++ build/closedTools/indexClosedTools.js
@@ -10,53 +10,44 @@ var _closedToolsSchema = require('./closedToolsSchema');
 
 var _closedToolsResolver = require('./closedToolsResolver');
 
-var _mongoose = require('mongoose');
-
-var _mongoose2 = _interopRequireDefault(_mongoose);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//const conectarDB = require('./dbConnection');
-//require('dotenv').config();
+/** 
+# indexClosedTools.js
+
+## Description
+Configures Graphql server
+
+## Usage
+```shell
+npm start
+```
+
+## Arguments/Parameters
+N/A
+
+## Examples
+N/A
+
+## Return 
+N/A
+
+## Category
+RegulonDB drawing traces tool web service
+
+## License
+
+## Author 
 
 
-// Make the connection to MongoDB using mongoose
-const uri = 'mongodb+srv://user:user@datamarts.iahyj.mongodb.net/regulondbdatamarts'; /** 
-                                                                                      # indexClosedTools.js
-                                                                                      
-                                                                                      ## Description
-                                                                                      Configures Graphql server
-                                                                                      
-                                                                                      ## Usage
-                                                                                      ```shell
-                                                                                      npm start
-                                                                                      ```
-                                                                                      
-                                                                                      ## Arguments/Parameters
-                                                                                      N/A
-                                                                                      
-                                                                                      ## Examples
-                                                                                      N/A
-                                                                                      
-                                                                                      ## Return 
-                                                                                      N/A
-                                                                                      
-                                                                                      ## Category
-                                                                                      RegulonDB drawing traces tool web service
-                                                                                      
-                                                                                      ## License
-                                                                                      
-                                                                                      ## Author 
-                                                                                      
-                                                                                      
-                                                                                      **/
+**/
 
 // imports needed libraries
+const conectarDB = require('./dbConnection');
+require('dotenv').config();
 
-_mongoose2.default.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// Make the connection to MongoDB using mongoose
+conectarDB();
 
 // Defining graphql server
 const server = new _apolloServerExpress.ApolloServer({
@@ -68,21 +59,11 @@ const server = new _apolloServerExpress.ApolloServer({
 // create an instance of express to be used with ApolloServer
 const app = (0, _express2.default)();
 
-// set a limiter  to the api
-/*
-const apiLimiter = rateLimit({
-    windowsMs = 60000,
-    max = 1000,
-    message:{
-        message: 'To many request',
-        statusCode: '429'
-    }
-}); 
-app.use(apiLimiter);
-*/
-
 // apply express instance to apolloserver
 server.applyMiddleware({ app });
 
+//Set an enviroment variable for the port (4000 by default)
+const PORT = process.env.PORT || 4000;
+
 //Server start
-const servExpress = app.listen(4000, () => console.log(`El servidor esta funcionando en http://localhost:${servExpress.address().port}${server.graphqlPath}`));
+const servExpress = app.listen(PORT, () => console.log(`El servidor esta funcionando en http://localhost:${servExpress.address().port}${server.graphqlPath}`));
